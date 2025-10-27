bb0ba4876daa453bcaa8f285eec33a5be41e8d9f
Tanisha-1212 <tanishatetarwal11@gmail.com>
2025-10-27 17:36:39 +0530

    updated gitignore

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+app.use(express.json()); // for JSON requests
+app.use(express.text({ type: "*/*" })); // for raw text requests
+
+import dotenv from "dotenv";
+dotenv.config();
+connectDB();
+
+app.use(cors());
+app.use(cookieParser());
+
+import authRoutes from "./routes/authRoutes.js";
+import snippetRoutes from "./routes/snippetRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
+
+app.use('/api/auth', authRoutes);
+app.use('/api/ai', aiRoutes);
+app.use('/api/snippet', snippetRoutes);
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+
+export default app;
